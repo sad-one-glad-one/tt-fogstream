16d8aee75b06b84c57d61d4945abfaa947e3e017
Aleksey Silnov <sadonegladone4@gmail.com>
2023-10-02 01:03:20 +0700

    lint: fix

--- src/hooks/useColorTheme.jsx
+++ src/hooks/useColorTheme.jsx
@@ -1,23 +1,24 @@
 import React, { useState } from 'react'
 
-import { getDesignTokens } from '../theme'
-
 import { createTheme } from '@mui/material'
 
+import { getDesignTokens } from '../theme'
+
+
 export const useColorTheme = () => {
-    const [mode, setMode] = useState("light")
+  const [mode, setMode] = useState('light')
+    
+  const toggleColorMode = (mode) =>
+    setMode((prevMode) => (prevMode !== mode && mode))
 
-    const toggleColorMode = (mode) =>
-        setMode((prevMode) => (prevMode !== mode && mode))
+  const modifiedTheme = React.useMemo(
+    () => createTheme('custom', getDesignTokens(mode)),
+    [mode]
+  )
 
-    const modifiedTheme = React.useMemo(
-        () => createTheme('custom', getDesignTokens(mode)),
-        [mode]
-    )
-
-    return {
-        theme: modifiedTheme,
-        mode,
-        toggleColorMode,
-    }
+  return {
+    theme: modifiedTheme,
+    mode,
+    toggleColorMode,
+  }
 }
--- src/theme/ThemeContextProvider.jsx
+++ src/theme/ThemeContextProvider.jsx
@@ -1,23 +1,25 @@
 import { createContext, useContext } from 'react'
 
-import { useColorTheme } from '../hooks/useColorTheme'
-
 import { createTheme } from '@mui/material'
 
+import { useColorTheme } from '../hooks/useColorTheme'
+
+
 export const ThemeContext = createContext({
-    mode: "light",
-    toggleColorMode: () => {},
-    theme: createTheme(),
+  mode: 'light',
+  toggleColorMode: () => {},
+  theme: createTheme(),
 })
 
 export const ThemeContextProvider = ({ children }) => {
-    const value = useColorTheme()
+  const value = useColorTheme()
 
-    return (
-        <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
-    )
+    
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  )
 }
 
 export const useThemeContext = () => {
-    return useContext(ThemeContext)
+  return useContext(ThemeContext)
 }
